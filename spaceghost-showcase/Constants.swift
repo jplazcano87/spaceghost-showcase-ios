1682dbde75647acd8a6ef61125f13eec2fc271c4
SpaceGhost <jp.lazcano87@gmail.com>
2016-02-24 22:41:57 -0300

    Login UI for Showcase App

---
+++ spaceghost-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  spaceghost-showcase
+//
+//  Created by Juan Pablo Lazcano Candia on 24-02-16.
+//  Copyright © 2016 Juan Pablo Lazcano Candia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR : CGFloat = 157.0 / 255.0
